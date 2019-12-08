5ab32ed3c9d16867d98984bb298c6eef1e24d675
Benjamin <58241833+Papr3ka@users.noreply.github.com>
2019-12-08 15:59:56 -0500

    Update Cruncher.py

--- Cruncher.py
+++ Cruncher.py
@@ -3,7 +3,7 @@
 import math
 import os
 import platform
-from timeit import default_timer as timer
+import time
 from decimal import *
 from decimal import Decimal as dec
 def clear():
@@ -26,14 +26,26 @@ def factorial(x):
     return x
 def BBP_Pi():
     clear()
+    global x
+    global fin
+    count = 1000
+    fin = bool(0)
     x = 0
     pi = 0
-    tstart = timer()
+    if not(dis_ch_bool):
+        print("Calculating...")
+    tstart = time.perf_counter()
     for x in range(0,maxpr): 
         pi += dec((dec(1) / dec(16) ** dec(x)) * (dec(4) / (dec(8) * x + dec(1)) - dec(2) / (dec(8) * x + dec(4)) - dec(1) / (dec(8) * x + dec(5)) - dec(1) / (dec(8) * x + dec(6))))
-        print(pi)
+        if dis_ch_bool:
+            print(pi)
+        elif x == count:
+            percom = float(int((x / maxpr) * 100000) / 1000)
+            print(percom,r"%")
+            count += 500
     finalpi = dec(int(pi * dec(10 ** maxpr)) / dec(10 ** maxpr))
-    tend = timer()
+    tend = time.perf_counter()
+    fin = bool(1)
     clear()
     print("Done!\n")
     print(finalpi)
@@ -44,21 +56,32 @@ def BBP_Pi():
     stlength = len(str(finalpi)) - lensub
     print("\nLength",stlength)
     print("\nAccurate Precision",maxpr)
-    print("Time",tend - tstart)
+    print("Time",int((tend - tstart) * 10000) / 10000,"Seconds")
     if textfc == "Y" or textfc == "y":
         outFile = open("PI.txt", "w+")
         outFile.write(str(finalpi))
-        outFile.close() 
+        outFile.close()
 def Euler():
     clear()
+    global x
+    global fin
+    count = 1000
+    fin = bool(0)
     x = 0
     euler = 0
-    tstart = timer()
+    if not(dis_ch_bool):
+        print("Calculating...")
+    tstart = time.perf_counter()
     for x in range(0,maxpr):
         euler += dec(dec(1) / dec(factorial(int(x))))
-        print(euler)
+        if dis_ch_bool:
+            print(euler)
+        elif x == count:
+            percom = float(int((x / maxpr) * 100000) / 1000)
+            print(percom,r"%")
+            count += 500
     finaleuler = dec(int(euler * dec(10 ** maxpr)) / dec(10 ** maxpr))
-    tend = timer()
+    tend = time.perf_counter()
     clear()
     print("Done!\n")
     print(finaleuler)
@@ -69,7 +92,7 @@ def Euler():
     stlength = len(str(finaleuler)) - lensub
     print("\nLength",stlength)
     print("\nAccurate Precision",maxpr)
-    print("Time",tend - tstart)
+    print("Time",int((tend - tstart) * 10000) / 10000,"Seconds")
     if textfc == "Y" or textfc == "y":
         outFile = open("Euler.txt", "w+")
         outFile.write(str(finaleuler))
@@ -78,13 +101,13 @@ def rootcrunch():
     root = dec(input("Root:"))
     x = dec(input("Root of:"))
     clear()
-    print("Calculating")
-    tstart = timer()
+    print("Calculating...")
+    tstart = time.perf_counter()
     sq = dec(x ** dec(dec(1) / dec(root)))
     clear()
     print("Converting")
     finalsq = dec(int(sq * dec(10 ** maxpr)) / dec(10 ** maxpr))
-    end = timer()
+    tend = time.perf_counter()
     clear()
     print("Done!\n")
     print(finalsq)
@@ -95,16 +118,16 @@ def rootcrunch():
     stlength = len(str(finalsq)) - sqvar
     print("\nLength",stlength)
     print("\nAccurate Precision",maxpr)
-    print("Time",tend - tstart)
+    print("Time",int((tend - tstart) * 10000) / 10000,"Seconds")
 def Gratio():
     clear()
-    print("Calculating")
-    tstart = timer()
+    print("Calculating...")
+    tstart = time.perf_counter()
     Golden = dec(dec(dec(1) + dec(dec(5) ** dec(0.5))) / dec(2))
     clear()
     print("Converting")
     finalgolden = dec(int(Golden * dec(10 ** maxpr)) / dec(10 ** maxpr))
-    tend = timer()
+    tend = time.perf_counter()
     clear()
     print("Done!\n")
     print(finalgolden)
@@ -115,11 +138,14 @@ def Gratio():
     stlength = len(str(finalgolden)) - lensub
     print("\nLength",stlength)
     print("\nAccurate Precision",maxpr)
-    print("\nTime",tend - tstart)
+    print("Time",int((tend - tstart) * 10000) / 10000,"Seconds")
 textfc = "n"
+dis_ch_var = "n"
+dis_ch_bool = 0
 waitsetc = 1
 maxpr = 0
 lensub = 2
+fin = bool(0)
 clear()
 print(r"Select Constant to compute")
 print("1:PI\n2:Euler\n3:Root\n4:Golden Ratio")
@@ -130,20 +156,24 @@ if option < 5 and option > 0:
 if option == 1 or option == 2:
     print(r"Output Text file Y/N")
     textfc = str(input())
+    print(r"Display Interval Y/N")
+    dis_ch_var = str(input())
+    if dis_ch_var == "Y" or dis_ch_var == "y":
+        dis_ch_bool = bool(1)
     wait()
 if option == 1:
     getcontext().prec = maxpr + 32
     BBP_Pi()
-if option == 2:
+elif option == 2:
     getcontext().prec = maxpr + 32
     Euler()
-if option == 3:
+elif option == 3:
     getcontext().prec = maxpr + 16
     rootcrunch()
-if option == 4:
+elif option == 4:
     getcontext().prec = maxpr + 8
     Gratio()
-if option > 4 or option < 1:
+else:
     clear()
     print("Invalid Option")
 if maxpr < 0:
